3a1d1b5dcc436d789a2fb57ad6b169db60f77d97
adirgit <adir.s1@gmail.com>
2016-10-28 16:52:52 +0300

    add file-directory-template

---
+++ HomeWork0/cookbooks/my-first-cookbook/recipes/task1.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: my-first-cookbook
+# Recipe:: task1
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+directory '/opt/opsSchool' do
+  action :create
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+# cookbook_file '/opt/opsSchool/test-task1.txt' do
+  # action :create
+  # owner 'root'
+  # group 'root'
+# end
+
+template '/opt/opsSchool/test-task1.txt' do
+  source 'test-task1.erb'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+package 'unzip' do
+  version '6.0-9ubuntu1.5'
+  action :install
+end
